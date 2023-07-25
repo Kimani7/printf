5932aa7144cd2cf698bbcbcfdcf2a31dd6264b45
Kimani7 <newtonkipesa@gmail.com>
2023-07-25 10:59:37 -0700

    printf project

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
@@ -115,3 +115,4 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
+
